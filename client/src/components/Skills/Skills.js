2e49cd765ebb5f34080daf2bda87d05b1a3ec7f2
Saeed Kolivand <saeedkolivand1997@gmail.com>
2019-09-26 15:21:01 +0330

    Skills Component Updated
    UI and Some JS codes added

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -1,13 +1,46 @@
 import React, {Component} from 'react';
-
 import './Skills.css';
-
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
 
 class Skills extends Component {
+
+    state = {
+        skills: [
+            {id: 1, skill: ''}
+        ]
+    };
+
+    handleAddNewSkillText = (event) => {
+        this.setState({
+            skills: [
+                {skill: event.target.value}
+            ]
+        })
+    };
+
+    handleAddSkill = (event) => {
+        event.preventDefault();
+        this.setState({
+            skills: {
+                skill: ''
+            }
+        });
+        console.log(this.state)
+    };
+
     render() {
         return (
-            <div id='skills'>
-                <p>مهارتتون رو وارد کنید</p>
+            <div id="skills">
+                <p>مهارت ها</p>
+                <form id="form-wrapper">
+                    <div className="addNewSkillBox">
+                        <Input changed={this.handleAddNewSkillText} value={this.state.skill} placeholder="مهارت خود را وارد کنید" autocomplete="off" type="text" name="skills" />
+                        <Button clicked={this.handleAddSkill} bgColor="#ff6666" color="white">
+                            +
+                        </Button>
+                    </div>
+                </form>
             </div>
         );
     }
